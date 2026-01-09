7d73d7880dba085668c2c7506137b77f265c50bd
chayan2006 <chayankhatua2006@gmail.com>
2026-01-09 11:49:37 +0530

    practing new Q

--- practiceQ.c
+++ practiceQ.c
@@ -13,14 +13,38 @@
 //     return 0;
 // }
 
+// # include <stdio.h>
+// int main(){
+//     //Q 1. Write a program to calculate area of a sqare.
+//     //(side is give)
+//     int side;
+//     printf("Enter the side of the square: ");
+//     scanf("%d",&side);
+//     int area = side*side;
+//     printf("The are of the side is %d",area);
+//     return 0;
+// }
+// #include <stdio.h>
+// int main(){
+//     //Q a. Write a program to calculate perimeter of rectangle .
+//     int lenght;
+//     printf("Enter the lenght of the rectangle: ");
+//     scanf("%d",&lenght);
+//     int breadth;
+//     printf("Enter the breadth of the rectangle: ");
+//     scanf("%d",&breadth);
+//     int perimeter = 2*(lenght + breadth);
+//     printf("The perimeter of the rectangle is %d",perimeter);
+//     return 0;
+//     }
 # include <stdio.h>
 int main(){
-    //Q 1. Write a program to calculate area of a sqare.
-    //(side is give)
-    int side;
-    printf("Enter the side of the square: ");
-    scanf("%d",&side);
-    int area = side*side;
-    printf("The are of the side is %d",area);
+    // Q take a number (n) from the user & output its cubes(n*n*n).
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    int cube = n*n*n;
+    printf("The cube of the number is %d",cube);
+
     return 0;
 }
