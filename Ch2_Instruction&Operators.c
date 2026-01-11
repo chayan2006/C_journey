d03163ec6e9fe44b3752eb6fd471b60e05ac0753
chayan2006 <chayankhatua2006@gmail.com>
2026-01-11 11:34:44 +0530

    adding new topic operaturs

--- Ch2_Instruction&Operators.c
+++ Ch2_Instruction&Operators.c
@@ -49,10 +49,42 @@ int main (){
     float op float = float
     */
    printf("%d\n", 5 + 2.5); // 2.5 is promoted to int (2) before addition, result is 7
-   printf("%.2f\n", 5 + 2.5); // 5 is promoted to float (5.0) before addition, result is 7.5    
+   printf("%.2f\n", 5 + 2.5); // 5 is promoted to float (5.0) before addition, result is 7.5  
+   /*
+   Arithmetic Instructions 
+   Operator Precedence 
+   *,/,%  (Higher Precedence)
+   +,-    (Lower Precedence)
+   1. a + b * c = a + (b * c)  // multiplication has higher precedence than addition
+   2. (a + b) * c = (a + b) * c  // parentheses override precedence
+   3. a + b - c + d = ((a + b) - c) + d  // left to right associativity for operators with same precedence
+   4. a * b / c * d = ((a * b) / c) * d  // left to right associativity for operators with same precedence
+   5. a + b % c = a + (b % c)  // modulus has higher precedence than addition
+   6. a % b * c = (a % b) * c  // left to right associativity for operators with same precedence
+   7. a + b * c - d / e = a + (b *  c) - (d / e)  // multiplication and division have higher precedence than addition and subtraction
+   8. a - b + c % d * e = a - b + ((c % d) * e)  // modulus and multiplication have higher precedence than addition and subtraction
+   9. (a + b) * (c - d) / e = ((a + b) * (c - d)) / e  // parentheses override precedence
+   10. a + (b - c) * d % e = a + (((b - c) * d) % e  // parentheses override precedence
+   11. a * (b + c) - d % e / f = (a * (b + c)) - ((d % e) / f)  // parentheses override precedence
+   12. a / b + c * d - e % f = (a / b) + (c * d) - (e % f)  // multiplication and division have higher precedence than addition and subtraction
+   13. a % b + c - d * e / f = (a % b) + c - ((d * e) / f)  // multiplication and division have higher precedence than addition and subtraction
+   14. (a - b) * (c + d) % e / f = (((a - b) * (c + d)) % e) / f  // parentheses override precedence
+   15. a + b - c * d % e / f = a + b - ((c * d) % e) / f  // multiplication and division have higher precedence than addition and subtraction
+   16. a * b + c - d % e / f = (a * b) + c - ((d % e) / f)  // multiplication and division have higher precedence than addition and subtraction
+   17. (a + b - c) * (d % e / f) = ((a + b - c) * (d % e / f))  // parentheses override precedence
+   18. a / b - c + d * e % f = (a / b) - c + ((d * e) % f)  // multiplication and division have higher precedence than addition and subtraction
+   19. a % b - c * d + e / f = (a % b) - (c * d) + (e / f)  // multiplication and division have higher precedence than addition and subtraction
+   20. (a * b + c) - (d / e % f) = ((a * b + c) - (d / e % f))  // parentheses override precedence
+   21. a + b * c - d + e / f % g = a   + (b * c) - d + ((e / f) % g)  // multiplication and division have higher precedence than addition and subtraction
+   */  
 }
 /*
 3. Control Instructions: These instructions control the flow of execution in a program. They include conditional statements and loops. For example:
    - if (a > b) { ... } // executes the block if 'a' is greater than 'b'
    - for (int i = 0; i < n; i++) { ... } // loops 'n' times
+   used to dtermine the flow of program execution based on certain conditions.
+   a. Sequence Control: Instructions are executed one after the other in the order they appear.
+   b. Decision Control: Instructions that allow branching based on conditions (e.g., if-else statements, switch-case).
+   c. Loop Control: Instructions that allow repeating a block of code multiple times (e.g., for, while, do-while loops).
+   d.Case Control: Instructions that allow multi-way branching based on the value of a variable (e.g., switch-case statements).
 */
