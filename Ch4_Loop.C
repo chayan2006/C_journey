a24349bbee9ecaa356bd4f811aab952e0aabe215
chayan2006 <chayankhatua2006@gmail.com>
2026-01-16 21:28:10 +0530

    started new chapter

---
+++ Ch4_Loop.C
@@ -0,0 +1,37 @@
+/*
+Loop Control Instructions in C 
+To repeat some part of code multiple times 
+There Are Three Loop Control Instructions in C 
+1. for Loop
+2. while Loop
+3. do while Loop
+
+*/
+// for loop 
+/*
+for (initialization ; condition ; increment/decrement){
+    // code to be executed repeatedly
+}
+*/
+#include <stdio.h>
+int main(){
+    // increment operator 
+    // ++i (pre-increment)
+    // i++ (post-increment)
+    // decrement operator
+    // --i (pre-decrement)
+    // i-- (post-decrement)
+    for (int   i= 0 ; i <= 5 ; i ++){
+        printf("Hello World i will sucessfully in this yeatr\n");
+    }
+    return 0;
+
+}
+// loop conture can be floated point value
+// infinite loop
+ /* 
+ While Loop 
+    while (condition){
+        // code to be executed repeatedly
+    }
+ */
---
+++ practiceQ_Ch4.c
@@ -0,0 +1,12 @@
+/*
+Q . Print the Numbers from 0 to 10 
+0 1 2 3 4  5 6 7 8 9 10
+
+*/
+#include <stdio.h>
+int main(){
+    for (int i = 0; i <= 10; i++){
+        printf("%d\n", i);
+    }
+    return 0;
+}
