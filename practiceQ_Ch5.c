01aba571fc3c6e28333d9311abb7a7d5e72dd016
chayan2006 <chayankhatua2006@gmail.com>
2026-01-25 17:37:00 +0530

    o

--- practiceQ_Ch5.c
+++ practiceQ_Ch5.c
@@ -3,6 +3,7 @@
 void printhello();
 void goodbye();
 
+/*
 int main (){
     printhello();
     goodbye();
@@ -14,3 +15,35 @@ void printhello(){
 void goodbye(){
     printf("Goodbye\n");
 }
+   */
+
+  
+// Q write a function that print Namaste if user is indian and Bonjour if user is french.
+void Namaste();
+void Bonjour();
+int main (){
+    char ch ; 
+    printf ("Enter I for indian and F for french\n");
+    scanf ("%c" , &ch);
+    if (ch == 'I' || ch == 'i'){
+        Namaste();
+    }
+    else if (ch == 'F' || ch == 'f'){
+        Bonjour();
+    }
+    else {
+        printf ("Invalid Input\n");
+    }
+}
+void Namaste ()
+    {
+      printf("Namaste\n");
+
+    }
+void Bonjour ()
+    {
+      printf("Bonjour\n");
+
+    }
+
+    
